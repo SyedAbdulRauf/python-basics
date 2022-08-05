c2bcbc196fd912351f86fa9b03b47820470fd733
SyedAbdulRauf <110671589+SyedAbdulRauf@users.noreply.github.com>
2022-08-05 16:57:32 +0500

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,8 @@
+
+
+box1="sayed"
+box2=11
+box3="sayedahmed25@gmail.com"
+score= 95.5
+
+print(type(box3))
---
+++ Basics2.py
@@ -0,0 +1,7 @@
+email=input("enter your emaill id: ")
+name=input("enter your name ")
+age=int(input("enter your age"))
+
+print(name)
+print(email)
+print(age)
